de264f8bd7b489d5eaccd52c79bf92d8b14a7494
AntonioAndradeGomes <gomesmax1997@gmail.com>
2024-02-05 23:39:57 -0300

    first commit

---
+++ src/components/note-card.tsx
@@ -0,0 +1,24 @@
+export function NoteCard() {
+  return (
+    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">
+        Há 2 dias
+      </span>
+      <p className="text-sm leading-6 text-slate-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+        dignissimos inventore doloremque omnis, voluptatem ipsum facere
+        laudantium cum, modi harum sed tempora corporis veritatis architecto
+        nulla delectus, molestiae dolor quos. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Labore dignissimos inventore doloremque
+        omnis, voluptatem ipsum facere laudantium cum, modi harum sed tempora
+        corporis veritatis architecto nulla delectus, molestiae dolor quos.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+        dignissimos inventore doloremque omnis, voluptatem ipsum facere
+        laudantium cum, modi harum sed tempora corporis veritatis architecto
+        nulla delectus, molestiae dolor quos.
+      </p>
+
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+    </button>
+  );
+}
